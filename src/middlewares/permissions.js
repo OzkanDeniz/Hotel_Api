74dca1db96769d4fae7c9a7b49de16da4473da39
ozkan <ozkandnz09@gmail.com>
2024-12-20 13:58:40 +0300

    permissions middleware

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -9,12 +9,13 @@ module.exports = {
       throw new Error("Nopermission : You must Login.");
     }
   },
+
   isAdmin: (req, res, next) => {
     if (req.user && req.user.isActive && req.user.isAdmin) {
       next();
     } else {
       res.errorStatusCode = 403;
-      throw new Error("Nopermission : You must Login and to be Admin");
+      throw new Error("Nopermission : You must Login and to be Admin.");
     }
   },
 };
