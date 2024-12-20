99519f26e365920b2a07911052b3af822db88ebc
ozkan <ozkandnz09@gmail.com>
2024-12-20 13:33:00 +0300

    permissions middleware

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -1,43 +1,20 @@
-'use strict'
+"use strict";
 
-module.exports ={
-
-isLogin : (req,res,next) => {
-    if(req.user && req.user.isActive){
-        next()
-    }else{
-     res.errorStatusCode = 403
-     throw new Error("Nopermission : You must Login.")  
+module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("Nopermission : You must Login.");
     }
-}
-,
-isAdmin : (req,res,next)=> {
-    if(req.user && req.user.isActive && req.user.isAdmin){
-        next()
-    }else{
-        res.errorStatusCode = 403
-        throw new Error("Nopermission : You must Login and to be Admin") 
+  },
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("Nopermission : You must Login and to be Admin");
     }
-},
-isAdminOrLead : (req,res,next)=>{
- if(
-    req.user&& req.user.isActive && (req.user.isAdmin || (req.user.isLead && req.user.departmentId === departmentId)) 
- ){
-    next()
- }else{
-    res.errorStatusCode= 403;
-    throw new Error("NoPermission: You must login to be Admin or DepartmentLead")
- }
-}
-
-
-
-
-
-
-
-
-
-
-
-}
+  },
+};
